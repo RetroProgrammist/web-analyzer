2fb493f3351c3e273cf247646fb69cb420beb833
Aliaksei Rzh <jobsprofileru@gmail.com>
2022-07-27 01:26:58 +0300

    v 1

--- script.js
+++ script.js
@@ -1,24 +1,79 @@
-const Wappalyzer = require('wappalyzer');
+import path from 'path';
+import * as fs from 'fs';
 
-const urls = ['https://www.wappalyzer.com', 'https://www.example.com']
+import {parse} from 'csv-parse';
+import Wappalyzer from 'wappalyzer';
+import normalizeUrl from 'normalize-url';
+import punycode from 'punycode/punycode.js';
 
-const wappalyzer = new Wappalyzer()
+let analyze = async function (inFilePath, outFilePath) {
 
-;(async function() {
+    let pos = 0;
+    let urls = [];
+    const options = {
+        maxWait: 5000,
+    };
+    const wappalyzer = new Wappalyzer(options);
+
     try {
-        await wappalyzer.init()
+        const parser = parse({delimiter: ';'}, function (err, data) {
+            data.forEach((val, index) => {
+                if (index === 0) {
+                    val.every((v, i) => {
+                        if (v === 'url') {
+                            pos = i;
+                            return false;
+                        }
+                        return true;
+                    });
+                    return;
+                }
+                urls.push(normalizeUrl(val[pos]));
+            });
+        });
+        parser.on('end', async function() {
+            console.log(urls);
+            await wappalyzer.init()
 
-        const results = await Promise.all(
-            urls.map(async (url) => ({
-                url,
-                results: await wappalyzer.open(url).analyze()
-            }))
-        )
+            const results = await Promise.all(
+                urls.map(async (url) => ({
+                    url,
+                    results: await wappalyzer.open(url).analyze()
+                }))
+            )
 
-        console.log(JSON.stringify(results, null, 2))
+            let format = path.parse(inFilePath);
+            let fPath = path.normalize(outFilePath+format.name+'.json');
+            !fs.existsSync(outFilePath) && fs.mkdirSync(outFilePath);
+
+            fs.appendFileSync(fPath, JSON.stringify(results, null, 2))
+            console.log("THE END");
+            await wappalyzer.destroy()
+        });
+
+
+        fs.createReadStream(inFilePath).pipe(parser);
     } catch (error) {
         console.error(error)
     }
+};
 
-    await wappalyzer.destroy()
-})()
+const inDirPath = path.normalize('./files/');
+const outDirPath = path.normalize('./results/');
+
+if (fs.existsSync(outDirPath)) {
+    fs.rmSync(outDirPath, {
+        recursive: true,
+    })
+}
+
+const files = fs.readdirSync(inDirPath, {withFileTypes: true});
+files.forEach((value)=>{
+    if(value.isFile() && path.extname(value.name) === '.csv') {
+        let fPath = path.join(inDirPath, value.name);
+        if(fs.existsSync(fPath)) {
+            analyze(fPath, outDirPath);
+        }
+    }
+});
+
